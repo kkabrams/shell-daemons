1c18b640f841520647ad34aca60c5cef5df2e51e
epoch <epoch@hack.thebackupbox.net>
2020-08-21 06:21:13 +0000

    added a little C program (yes, I know, this repo says "shell-daemons", but this is a helper program, not the "daemon" part) to extract a query string parameter value from QUERY_STRING env var

---
+++ query_param.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//this is to replace all of the:
+//   printf "%s\n" "$QUERY_STRING" | tr '&' '\n' | grep "^$1=" | cut -d= -f2-
+//that I do a lot.
+
+#include <stdlib.h> //getenv
+
+int main(int argc,char *argv[]) {
+  char *p;
+  if(argc != 2) return fprintf(stderr,"usage: query_param param_name\n"),1;
+  for(p=getenv("QUERY_STRING");p != 1;p=strchr(p,'&')+1) {
+    if(!strncmp(p,argv[1],strlen(argv[1]))) {
+      if(strchr(p,'=')-p == strlen(argv[1])) {
+        if(strchr(p,'&')) *strchr(p,'&')=0;
+        printf("%s\n",p+strlen(argv[1])+1);
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
